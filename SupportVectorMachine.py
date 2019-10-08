2ec05fdd03c96e155deea97f16ccbad9121a3024
Heber Ribeiro <heber.r@outlook.com>
2019-10-08 13:41:32 -0300

    Attribute Selection

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.svm import SVC
+from sklearn.ensemble import ExtraTreesClassifier
 
 # Carregamento base de dados
 dadosCredito = pd.read_csv('Credit.csv')
@@ -23,7 +24,7 @@ for i in atributosParaEncoder:
     dadosCredito[i] = labelencoder.fit_transform(
             dadosCredito[i])
 del i
-    
+
 # Definição dos atributos previsores e do atributo da classe
 previsores = dadosCredito.iloc[:, 0:20]#.values
 classe = dadosCredito.iloc[:, 20]#.values
@@ -42,3 +43,20 @@ previsoes = modelo_svm.predict(X_test)
 confusao = confusion_matrix(y_test, previsoes)
 taxaAcerto = accuracy_score(y_test, previsoes)
 taxaErro = 1 - taxaAcerto
+
+# Seleção de atributos
+forest = ExtraTreesClassifier(n_estimators=10)
+forest.fit(X_train,  y_train)
+impAttr = list( forest.feature_importances_)
+
+dict = { 'Atributos': [], 'Grau de Importancia do Atributo (%)': []}
+
+for i in range(len(impAttr)):
+    dict['Atributos'].append(X_train.columns[i])
+    dict['Grau de Importancia do Atributo (%)'].append( 
+            round( impAttr[i] * 100.0, 2 ) )
+del i
+grauImportanciaAtributos = pd.DataFrame(dict, columns=['Atributos', 'Grau de Importancia do Atributo (%)'])
+del dict
+grauImportanciaAtributos.sort_values(by='Grau de Importancia do Atributo (%)', 
+                                    ascending=False, inplace=True)
